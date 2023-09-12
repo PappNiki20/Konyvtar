07b64025f90d51a55c48e012141c258cb7c83fd5
PappNiki20 <papp.nikoletta001@gmail.com>
2023-09-12 14:22:52 +0200

    adatok femt

--- konyvtar/database/migrations/2014_10_12_000000_create_users_table.php
+++ konyvtar/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,6 +20,16 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+        User::create([
+         'name' => 'En',
+         'email'=>'edu@link.hu', 
+         'password'=>'Aa123']);
+
+         User::create([
+            'name' => 'Leila',
+            'email'=>'leila@link.hu', 
+            'password'=>'Aa123']);
+   
     }
 
     /**
